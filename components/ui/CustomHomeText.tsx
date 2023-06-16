a23baf8999de32bf31e8e6e48f4f00a6493f7b32
HamiltonLopes <hamilton_dino02@hotmail.com>
2023-06-15 23:26:05 -0300

    style: eslint changes

--- components/ui/CustomHomeText.tsx
+++ components/ui/CustomHomeText.tsx
@@ -4,20 +4,36 @@ function CustomHomeText() {
       <div class="text-2xl leading-8 lg:leading-10 text-base-content lg:text-4xl flex lg:w-full">
         <h1 class="t-heading-1 border-r pl-3 pr-3 text-sm lg:text-xl flex flex-col lg:flex-row lg:pr-[10px] lg:justify-end font-semibold w-1/2">
           <span>Lentes para</span>
-          <span> Óculos de Sol</span>
+          <span>Óculos de Sol</span>
         </h1>
         <p class="lh-copy border-l pl-[15px] pr-[15px] text-sm lg:text-xl lg:flex lg:pl-[10px] lg:justify-start w-1/2">
-          <a href="/lentes-para-oculos-de-sol/oakley" class="font-semibold" title="Lentes para Oakley">
+          <a
+            href="/lentes-para-oculos-de-sol/oakley"
+            class="font-semibold"
+            title="Lentes para Oakley"
+          >
             Oakley
           </a>,{" "}
-          <a href="/lentes-para-oculos-de-sol/ray-ban" class="font-semibold" title="Lentes para Ray-Ban">
+          <a
+            href="/lentes-para-oculos-de-sol/ray-ban"
+            class="font-semibold"
+            title="Lentes para Ray-Ban"
+          >
             Ray-Ban
           </a>,{" "}
-          <a href="/lentes-para-oculos-de-sol/mormaii" class="font-semibold" title="Lentes para Mormaii">
+          <a
+            href="/lentes-para-oculos-de-sol/mormaii"
+            class="font-semibold"
+            title="Lentes para Mormaii"
+          >
             Mormaii
           </a>{" "}
           e{" "}
-          <a href="/lentes-para-oculos-de-sol/evoke" class="font-semibold" title="Lentes para Evoke">
+          <a
+            href="/lentes-para-oculos-de-sol/evoke"
+            class="font-semibold"
+            title="Lentes para Evoke"
+          >
             Evoke.
           </a>
         </p>
